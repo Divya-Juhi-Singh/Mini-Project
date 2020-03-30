4d3bac01f2a22a806c6d13afda748a7275e1f34e
saloniig <56845649+saloniig@users.noreply.github.com>
2020-03-30 17:07:36 +0530

    Update data.py

--- data.py
+++ data.py
@@ -23,7 +23,6 @@ with open('ApplicationFormExam.csv', 'r') as file:
 		SubjectCode2=row[14]
 		SubjectName2=row[15]
 		Photo=row[16]
-#print(DateTime, CRN,Programme,Branch,AdmissionYear,URN,Semester,NameSelf,NameFather,NameMother,MobileNumber,Email,SubjectCode1,SubjectName1,SubjectCode2,SubjectName2,Photo)
 
 		with open("form1.html",'r',encoding='utf-8')as f:
 			var=f.read()
@@ -41,7 +40,6 @@ with open('ApplicationFormExam.csv', 'r') as file:
 			var = var.replace("_SubjectCode1_",SubjectCode1)
 			var = var.replace("_SubjectCode2_",SubjectCode2)
 
-			print(var)
 		with open(URN+".html", "w") as outf:
 			outf.write(str(var))
 
